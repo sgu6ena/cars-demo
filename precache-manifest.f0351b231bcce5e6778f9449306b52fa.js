a14c50a59362bab47ce082e89feb15e0f28cc78a
Natalja Issar <78623661+sgu6ena@users.noreply.github.com>
2022-01-06 10:30:58 +0200

    Add files via upload
    init

---
+++ precache-manifest.f0351b231bcce5e6778f9449306b52fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "223c24673619276d7da9",
+    "url": "/static/css/main.9c68c67e.chunk.css"
+  },
+  {
+    "revision": "223c24673619276d7da9",
+    "url": "/static/js/main.223c2467.chunk.js"
+  },
+  {
+    "revision": "0a7fcb0aa1e6a04e2e8a",
+    "url": "/static/js/1.0a7fcb0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b7c50342fb6fa73f9a8eb995b943a69",
+    "url": "/index.html"
+  }
+];
